902fb55725fc70c32d798768721b3b7bc3a99b10
Cheonhwa Kim <trigonotis97@gmail.com>
2020-07-02 23:04:26 +0900

    프로젝트 폴더위치 변경.  npm으로 라이브러리들 정적으로 추가
    추가한 라이브러리 : jquery, bootstrap, font-awesome, jdenticon

--- src/main/java/me/cheonhwa/bookyouthspace/config/SecurityConfig.java
+++ src/main/java/me/cheonhwa/bookyouthspace/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package me.cheonhwa.bookyouthspace.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -13,9 +15,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .mvcMatchers("/","/hello").permitAll()
+                .mvcMatchers("/", "/hello").permitAll()
                 .anyRequest().authenticated();
 
         //super.configure(http);
     }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        //super.configure(web);
+        web.ignoring()
+                .mvcMatchers("/node_modules/**")
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
 }
+
